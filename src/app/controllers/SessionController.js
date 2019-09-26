d093888aa6ff2ecc7160f6898dacdc6c0b55dd28
lukassxp <lpalucas.10@gmail.com>
2019-09-26 15:47:22 -0300

    is24 - is25 - is26 - removed validations of inside of controllers
    Signed-off-by: lukassxp <lpalucas.10@gmail.com>
    Co-authored-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Co-authored-by: lucassiqz <lucassiqzro@gmail.com>

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,3 @@
-import * as Yup from 'yup';
 import jwt from 'jsonwebtoken';
 
 import User from '../schemas/User';
@@ -6,19 +5,6 @@ import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res
-        .status(400)
-        .json({ error: 'Body of request out of expected format!' });
-    }
-
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,21 @@ import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
+import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
+import validateSessionStore from './app/validators/SessionStore';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validateUserStore, UserController.store);
+routes.post('/sessions', validateSessionStore, SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.put('/users', validateUserUpdate, UserController.update);
 
 export default routes;
